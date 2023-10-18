f8eade3e1036b74c378994c5a83dee3523d7d0e7
Leewonno <dldnjssh123@naver.com>
2023-10-18 17:31:07 +0900

    서버 수정

---
+++ server/config/config.js
@@ -0,0 +1,27 @@
+const development = {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PW,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "dialect": "mysql",
+    "timezone": "Asia/Seoul",
+}
+
+const test = {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PW,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "dialect": "mysql"
+}
+
+const production = {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PW,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "dialect": "mysql"
+}
+
+module.exports = { development, production, test };
+  
