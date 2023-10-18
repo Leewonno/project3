e9065aa839955aeac85204ff3b2b839cf25e20ee
Leewonno <dldnjssh123@naver.com>
2023-10-18 17:31:07 +0900

    서버 수정

--- server/models/index.js
+++ server/models/index.js
@@ -2,8 +2,10 @@
 
 
 const Sequelize = require('sequelize');
-const config = require(__dirname + '/../config/config.json')["development"];
+require('dotenv').config();
+const config = require(__dirname + '/../config/config.js')["development"];
 const db = {};
+
 
 let sequelize = new Sequelize(config.database, config.username, config.password, config);
 
