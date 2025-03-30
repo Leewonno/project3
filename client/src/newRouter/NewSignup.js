cbfb35ea662c204233c1f12d6df6c244a937f5ba
Leewonno <dldnjssh123@naver.com>
2025-03-30 22:02:08 +0900

    [Feat] signin signup

--- client/src/newRouter/NewSignup.js
+++ client/src/newRouter/NewSignup.js
@@ -1,10 +1,12 @@
 import SingupBox from '../organism/SignupBox'
 import signup from "../css/signup.module.css"
 
-export default function NewSignup(){
+export default function NewSignup() {
 
     return <>
-        <div className={signup.title}>회원가입</div>
-        <SingupBox />
+        <div style={{'width': '100%', 'padding': '20px', 'boxSizing': 'border-box', 'display': 'flex', 'justifyContent': 'center', 'flexDirection': 'column', 'alignItems': 'center'}}>
+            <div className={signup.title}>회원가입</div>
+            <SingupBox />
+        </div>
     </>
 }
