818a5a1d7671abfc4799e69b904e40e533064725
Leewonno <dldnjssh123@naver.com>
2025-03-30 18:20:34 +0900

    [Feat] main 반응형

--- client/src/newRouter/NewMain.js
+++ client/src/newRouter/NewMain.js
@@ -89,7 +89,7 @@ export default function NewMain() {
             <div className={main.popularBox}>
               {popularNovel.map((value, index) => {
                 return (
-                  <A url={"/n/" + value.title} key={index}>
+                  <A url={"/n/" + value.title} key={index} style={{'width':'100%'}}>
                     <div className={main.popularItem}>
                       <div className={main.popularCoverImgBox}>
                         <img className={main.popularCoverImg} src={value.cover_img} alt="커버이미지"></img>
