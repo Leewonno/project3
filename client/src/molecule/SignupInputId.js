bf449e6dd28b8d7c1a370d8baacdce01596600f0
Leewonno <dldnjssh123@naver.com>
2025-05-10 19:35:17 +0900

    [Feat] signup validation

--- client/src/molecule/SignupInputId.js
+++ client/src/molecule/SignupInputId.js
@@ -7,8 +7,10 @@ import signup from "../css/signup.module.css"
 export default function SignupInputId() {
 
     const value = useContext(SignupContext);
+    const { idCheck, idInput } = value;
 
     return <>
         <Input placeholder="아이디" className={signup.input} value={value.idInput} onChange={(e) => value.setIdInput(e.target.value)}></Input>
+        {!idCheck && idInput.length > 0 ? <span className={signup.alarmText}>아이디는 이메일 형식으로 입력해주세요.</span> : <></>}
     </>
 }
